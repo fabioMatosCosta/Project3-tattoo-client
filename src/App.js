b15036c7b059f695c0c90fd6ae905ad79b1c2602
Fábio Costa <fabiodanielmc@gmail.com>
2020-04-09 16:59:34 +0200

    profile started

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Switch, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
+import Profile from './pages/Profile';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route exact path = '/' component = {Home}/>
           <Route path = '/signup' component = {Signup}/>
           <Route path = '/login' component = {Login}/>
+          <Route path = '/profile' component = {Profile}/>
         </Switch>
     </div>
   );
