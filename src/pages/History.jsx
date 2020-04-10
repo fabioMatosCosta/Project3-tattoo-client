a5bf0e43a139110325237196f9865419cd5a2336
unknown <damianosmakris@hotmail.com>
2020-04-10 16:57:11 +0200

    something

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import DefaultLayout from "../layout/Default";
+import {Link} from "react-router-dom"
 
 class History extends Component {
     constructor(props) {
@@ -13,17 +14,11 @@ class History extends Component {
     render() {
         return (
             <DefaultLayout>
-                <div class="columns ">
-                    <div class="column">
-                        <div>
-                            <img src="https://source.unsplash.com/random" alt="" />
-                        </div>
-                        <div>
-                            <p>Blackest Black Tattoo's</p>
-                            <button>Find out more</button>
-                        </div>
-                    </div>
-                    </div>
+                <div>
+                    <Link to="/museum">
+                     Tattoo History Museum
+                    </Link>
+                </div>
             </DefaultLayout>
         )
     }
---
+++ src/pages/Museum.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+class Museum extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+                 
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <p>When and Where Were Tattoos First Performed?
+                    <br/>
+Tattoos date back many thousands of years.
+ In fact, we have firm evidence that tattooing is an ancient art form, after discoveries of tattoos on mummified skin were found.
+  The oldest evidence of human tattoos is believed to be from between 3370 BC and 3100 BC.
+
+Otzi the Iceman was discovered in September 1991.
+ His nickname comes from the location he was found in the Otzal Alps. 
+ His body has naturally mummified and preserved, making him Europe’s oldest human mummy.</p>
+ <img src="https://d3eh3svpl1busq.cloudfront.net/hKwyKHaPYdQPertjJeptpCwDngTecnnQ/assets/static/optimized/rev-d9bde98/wp-content/uploads/2018/10/Otzi-Iceman-Mummy.jpg" alt=""/>
+            </div>
+        )
+    }
+}
+
+export default Museum
