688bbb70efc7c489ef55adcdd00edba59636c41b
Fábio Costa <fabiodanielmc@gmail.com>
2020-04-16 21:38:49 +0200

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ function Footer() {
     return (
         <footer>
             <hr/>
-            <small>What ? You are also interested in music ?  Check this website developed by us: <a href="http://lulabazooka.herokuapp.com/ ">Lulabazooka</a></small>
+            <small id = "ft1">What ? You are also interested in music ?  Check this website developed by us: <a href="http://lulabazooka.herokuapp.com/ ">Lulabazooka</a></small>
             <br/>
-            <small>© Copyright 2020 Damianos and Fábio</small>
+            <small id = "ft2">© Copyright 2020 Damianos and Fábio</small>
         </footer>
     )
 }
