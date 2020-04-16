8acd7d3073f4fd00d7c743e0be9a249e0a8b09c4
Fábio Costa <fabiodanielmc@gmail.com>
2020-04-16 18:07:23 +0200

    solved merge

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,11 +2,7 @@ import React, { Component } from 'react'
 import { getUser, profile } from '../utils/auth';
 import { Link } from "react-router-dom"
 import DefaultLayout from "../layout/Default";
-<<<<<<< HEAD
 import "./Profile.scss"
-=======
-import './Profile.scss'
->>>>>>> b4cac24fe57e2c2c9879d307325aba433368a9a4
 
 class Profile extends Component {
     constructor(props) {
@@ -22,14 +18,9 @@ class Profile extends Component {
     render() {
         return (
             <DefaultLayout>
-<<<<<<< HEAD
                 <h1 className="title is-3">Yo {this.state.user.firstName.toUpperCase()}</h1>
                 <div className="back">
                 <h1>Yo {this.state.user.firstName.toUpperCase()}</h1>
-=======
-                <div className="back">
-                <h1>Yo {this.state.user.firstName}</h1>
->>>>>>> b4cac24fe57e2c2c9879d307325aba433368a9a4
 
                     <figure className = "image is-1by1">
                         <img className="is-rounded" src={this.state.user.image.imgPath} alt="pic"/>
