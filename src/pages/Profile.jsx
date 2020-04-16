9d182490dc8eff2fe3a4191bf9ec15c26ca3c5de
unknown <damianosmakris@hotmail.com>
2020-04-16 15:43:08 +0200

    merge

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import {getUser, profile} from '../utils/auth';
-import {Link} from "react-router-dom"
+import { getUser, profile } from '../utils/auth';
+import { Link } from "react-router-dom"
 import DefaultLayout from "../layout/Default";
 import "./Profile.scss"
 
@@ -13,22 +13,22 @@ class Profile extends Component {
             user: getUser()
         }
     }
-    
+
 
     render() {
         return (
             <DefaultLayout>
                 <div className="back">
-                <h1>Yo {this.state.user.firstName}</h1>
+                    <h1>Yo {this.state.user.firstName}</h1>
 
-                    <figure className = "image is-128x128">
-                        <img className="is-rounded" src={this.state.user.image.imgPath} alt="pic"/>
+                    <figure className="image is-128x128">
+                        <img className="is-rounded" src={this.state.user.image.imgPath} alt="pic" />
                     </figure>
                     <div id="container">
-  <p><Link to = "/edit-profile">Edit Profile Pic</Link>
-
-  </p>
-</div>                </div>
+                        <p><Link to="/edit-profile">Edit Profile Pic</Link>
+                        </p>
+                    </div>
+                    </div>
             </DefaultLayout>
         )
     }
