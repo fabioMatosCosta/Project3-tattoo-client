4cdb7f0ac06436ce8abfcb5a66b00f582f9553eb
unknown <damianosmakris@hotmail.com>
2020-04-16 15:38:02 +0200

    deploying a little

--- src/layout/Default.js
+++ src/layout/Default.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Nav from "../components/Nav";
-
+import ArtNav from "../components/ArtNav";
+import {getUser} from '../utils/auth';
 function Default(props) {
+    const user = getUser();
     return (
         <div>
-            <Nav />
+            {user?
+                <Nav />
+            :
+                <ArtNav />
+            }
             {props.children}
         </div>
     )
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import {getUser, profile} from '../utils/auth';
 import {Link} from "react-router-dom"
 import DefaultLayout from "../layout/Default";
-
+import "./Profile.scss"
 
 class Profile extends Component {
     constructor(props) {
@@ -18,13 +18,17 @@ class Profile extends Component {
     render() {
         return (
             <DefaultLayout>
+                <div className="back">
                 <h1>Yo {this.state.user.firstName}</h1>
 
                     <figure className = "image is-128x128">
                         <img className="is-rounded" src={this.state.user.image.imgPath} alt="pic"/>
                     </figure>
+                    <div id="container">
+  <p><Link to = "/edit-profile">Edit Profile Pic</Link>
 
-                <Link to = "/edit-profile"><button>Edit Profile Pic</button></Link>
+  </p>
+</div>                </div>
             </DefaultLayout>
         )
     }
